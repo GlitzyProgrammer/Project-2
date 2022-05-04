5778d860a2524acc8fa7f190571e0a055fcf2fcb
GlitzyProgrammer <99457791+GlitzyProgrammer@users.noreply.github.com>
2022-05-04 11:29:13 -0500

    This is a more updated version

--- Scheduler.py
+++ Scheduler.py
@@ -5,20 +5,25 @@ class schedule_widget:
     """
     This class is used to create the object scheduluar
     """
+
     def __init__(self,window):
         """
         this intializes all the widgets that the class calls upon for it's functionality
         :param window:
         """
-
+        month = ""
+        year = ""
+        day = ""
+        event = ""
         self.window = window
         self.Hello = Label(window,text='Welcome to the schedule program!\nDo you want to modify a schedule?',font=('Times New Roman',14))
         self.ExampleText = Label(window,text='Example of what to input in the text boxes.\nMay\n31\n2023\nEating with Sally')
         self.Entry = Entry(window)
-        self.FoundLabel = Label(window,text='We found ')
+        self.FoundLabel = Label(window,text=f'We found the date Month:{month}\nDay:{day}\nYear:{year}\nEvent:{event}')
         self.YesButton = Button(window,text='YES',command=self.yesoption, font=('Times New Roman',14))
         self.NoButton = Button(window, text='NO',command=self.no_option, font=('Times New Roman',14))
-        self.SearchInstruction = Label(window, text='To find your event just\nenter Month,Day,Year',font=('Times New Roman',10))
+        self.SearchInstruction = Label(window, text='To find or delete your event just\nenter Month,Day,Year',font=('Times New Roman',10))
+        self.DeleteButton = Button(window, text='DELETE',command=self.delete_command, font=('Times New Roman',14))
         self.SearchSubmit = Button(window,command=self.data_search, text= 'Submit')
         self.QuitButton = Button(window,text= 'Quit',command=window.destroy, font=('Times New Roman',14))
         self.Instruction = Label(window, text='Enter a Month, Day, Year, Event', font=('Times New Roman',12))
@@ -61,6 +66,7 @@ class schedule_widget:
         self.EventEntry.grid(row=4,column=1)
         self.SubmitButton.grid(row=5,column=0)
         self.QuitButton.grid(row=6,column=0)
+        self.BackButton.grid(row=7, column=0)
 
     def dataRetrival(self):
         """
@@ -134,11 +140,13 @@ class schedule_widget:
         self.SubmitButton.grid_remove()
         self.SearchSubmit.grid_remove()
         self.QuitButton.grid_remove()
+        self.DeleteButton.grid_remove()
         self.SearchInstruction.grid_remove()
         self.NoButton.grid()
         self.YesButton.grid()
         self.Hello.grid()
         self.ExampleText.grid()
+        self.QuitButton.grid()
 
     def no_option(self):
         """
@@ -159,6 +167,10 @@ class schedule_widget:
         self.YearEntry.grid(row=4, column=1)
         self.SearchSubmit.grid(row=5,column=0)
         self.QuitButton.grid(row=6,column=0)
+        self.BackButton.grid(row=7, column=0)
+        self.FoundLabel.grid(row=7, column=1)
+        self.DeleteButton.grid(row=8, column=0)
+        self.FoundLabel.grid_remove()
 
     def data_search(self):
         month = self.MonthEntry.get().lower().strip()
@@ -198,10 +210,14 @@ class schedule_widget:
             self.MonthEntry.delete(0, END)
             self.DayEntry.delete(0, END)
             self.YearEntry.delete(0, END)
+            occurance = 0
             for line in file:
+                #TODO: Eventaully reduce time compelxedity cause it's currently O(n) (fine but better exsit)
                 if reference in line:
-                   # TODO: Make a label that does display the month,day,event,year variables
-                    self.FoundLabel.grid(row=7,column=0)
+                    self.FoundLabel.grid()
+                    break
+                # TODO: Make a label that does display the month,day,event,year variables
+
 
 
     def delay(self):
@@ -210,3 +226,55 @@ class schedule_widget:
         :return:
         """
         TIME.sleep(1.5)
+
+    def delete_command(self):
+        """
+        This is like the data_search method but this time it removes the item when contitionds are fufilled.
+        :return:
+        """
+        month = self.MonthEntry.get().lower().strip()
+        day = self.DayEntry.get().strip()
+        year = self.YearEntry.get().strip()
+        try:
+            if year.isdigit():
+                year = int(year)
+                if year <= 9999:
+                    year = str(year)
+
+                else:
+                    raise ValueError
+            else:
+                raise ValueError
+
+            if not (month.isdigit()):
+                if month in Calendar.month:
+                    month = month
+                else:
+                    raise ValueError
+            else:
+                raise ValueError
+            if day.isdigit():
+                day = int(day)
+                if day <= 31 and day > 0:
+                    day = str(day)
+                else:
+                    raise ValueError
+            else:
+                raise ValueError
+        except ValueError:
+            self.ErrorMessage.grid(row=6, column=0)
+            self.BackButton.grid(row=7, column=0)
+        with open("Schedule_Book.txt", 'r+', newline='') as file:
+            reference = month + day + year
+            self.MonthEntry.delete(0, END)
+            self.DayEntry.delete(0, END)
+            self.YearEntry.delete(0, END)
+            line = file.readlines()
+            occurance = 0
+            for line in file:
+                # TODO: Eventaully reduce time compelxedity cause it's currently O(n) (fine but better exsit)
+               #  TODO: Figure out how to detete the item and doesn't leave a blank line
+                if reference in line:
+                    file.truncate(4)
+                    break
+                # TODO: Make a label that does display the month,day,event,year variables
